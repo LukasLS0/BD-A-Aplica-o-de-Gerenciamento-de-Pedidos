cee097fcdaaec2d2421ad6c742bf8ed858025aea
LukasLS0 <lukaslimasodre2@gmail.com>
2026-04-24 12:00:01 -0300

    feat(urls): add rotas para vendedor e cliente

--- amazon/amazon/urls.py
+++ amazon/amazon/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from backend import views
 
-
 router = DefaultRouter()
 router.register(r'clientes', views.ClienteViewSet, basename='cliente')
+router.register(r'vendedores', views.VendedorViewSet, basename='vendedor')
+router.register(r'produtos', views.ProdutoViewSet, basename='produto')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
