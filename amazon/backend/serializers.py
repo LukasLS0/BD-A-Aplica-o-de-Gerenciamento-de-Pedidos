777971677aedece5edb14e54f6e9bf6fe2d18b5a
LukasLS0 <lukaslimasodre2@gmail.com>
2026-04-24 11:50:05 -0300

    feat(serializers): add serializer de vendedor e produto

--- amazon/backend/serializers.py
+++ amazon/backend/serializers.py
@@ -1,5 +1,7 @@
+from typing import ReadOnly
+from django.forms import fields
 from rest_framework import serializers
-from .models import Cliente
+from .models import Cliente, Produto, Vendendor
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,3 +11,16 @@ class ClienteSerializer(serializers.ModelSerializer):
         # fields = ['id, 'nome', 'email']
         # Para excluir campos, use:
         # exclude = ['data_cadastro']
+
+class VendedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendendor
+        fields = '__all__'
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    categoria_display = serializers.CharField(source='get_categoria_display', read_only=True)
+
+    class Meta:
+        model = Produto
+        fields = '__all__' 
+        extra_fields = ['categoria_display']
