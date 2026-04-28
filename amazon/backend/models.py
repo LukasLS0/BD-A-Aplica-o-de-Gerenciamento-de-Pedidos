80749c139ab2e65a5ed5242bb8e0f7cb465614f4
LukasLS0 <lukaslimasodre2@gmail.com>
2026-04-28 08:56:28 -0300

    feat: add models de Endereco, FormaPagamento, Item, Pedido

--- amazon/backend/models.py
+++ amazon/backend/models.py
@@ -58,3 +58,63 @@ class Produto(models.Model):
         return f"{self.nome} - R$ {self.preco}"
 
 
+class Endereco(models.Model):
+    ## Precisa fazer choices para o estado? cidade?
+    ## cep
+    #cliente id
+    rua = models.CharField(max_length=255, null=False, blank=False)
+    cidade = models.CharField(max_length=100, null=False, blank=False)
+    estado = models.CharField(max_length=50, null=False, blank=False)
+    cep = models.CharField(max_length=10, null=False, blank=False)
+
+    class Meta:
+        db_table = 'enderecos'
+        ordering = ['estado']
+
+    def __str__(self):
+        return f"{self.rua}, {self.cidade}-{self.estado}"
+
+class FormaPagamento(models.Model):
+    ## Precisa fazer o choices para as formas de pagamentos aceitadas
+    tipo = models.CharField(max_length=50, null=False, blank=False)
+
+    class Meta:
+        db_table = 'formas_pagamentos'
+        ordering = ['tipo']
+
+    def __str__(self):
+        return f"{self.tipo}"
+
+class Item(models.Model):
+    # fazer choices de categoria
+    # vendedor id
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    descricao = models.TextField(blank=True)
+    categoria = models.CharField(max_length=50, blank=True)
+    quantidade_estoque = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'item'
+        ordering = ['nome']
+
+    def __str__(self):
+        return f"{self.nome} - {self.categoria}"
+
+class Pedido(models.Model):
+    #cliente id
+    #endereco id
+    #forma pagamento
+    data = models.DateField()
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'pedidos'
+        ordering = ['data']
+
+    def __str__(self):
+        return f"{self.data} - {self.status}"
+
+## devo fazer item_pedido?
+
